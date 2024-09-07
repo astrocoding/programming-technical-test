e1d643c922f8ef95bde0a10134ac6b3d431401a4
astrocoding <zaenalalfian20@gmail.com>
2024-09-08 00:58:06 +0700

    feat: add algorithm for N-Queen Problem

---
+++ java/NQueenProblem.java
@@ -0,0 +1,57 @@
+public class NQueenProblem {
+  private static int N;
+
+  public static void solveNQueens(int n) {
+    N = n;
+    int[][] board = new int[N][N];
+    if (solveNQueensUtil(board, 0)) {
+      printSolution(board);
+    } else {
+      System.out.println("Solusi tidak ditemukan.");
+    }
+  }
+
+  private static boolean solveNQueensUtil(int[][] board, int col) {
+    if (col >= N) return true;
+
+    for (int i = 0; i < N; i++) {
+      if (isSafe(board, i, col)) {
+        board[i][col] = 1;
+        if (solveNQueensUtil(board, col + 1)) return true;
+        board[i][col] = 0;
+      }
+    }
+
+    return false;
+  }
+
+  private static boolean isSafe(int[][] board, int row, int col) {
+    for (int i = 0; i < col; i++) {
+      if (board[row][i] == 1) return false;
+    }
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] == 1) return false;
+    }
+
+    for (int i = row, j = col; i < N && j >= 0; i++, j--) {
+      if (board[i][j] == 1) return false;
+    }
+
+    return true;
+  }
+
+  private static void printSolution(int[][] board) {
+    for (int[] row : board) {
+      for (int cell : row) {
+        System.out.print(cell + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int n = 5;
+    solveNQueens(n);
+  }
+}
