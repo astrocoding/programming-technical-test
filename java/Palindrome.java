10e62ab750b94eeb724ceb33dfa9ba2c07f30f4e
astrocoding <zaenalalfian20@gmail.com>
2024-09-08 00:53:51 +0700

    fix: modified palindrome checker logic

--- java/Palindrome.java
+++ java/Palindrome.java
@@ -3,28 +3,19 @@ import java.util.Scanner;
 public class Palindrome {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a string: ");
+    System.out.print("Masukkan sebuah kata: ");
     String input = scanner.nextLine();
     scanner.close();
 
     boolean isPalindrome = checkPalindrome(input);
-    int characterCount = countCharacters(input);
+    int characterCount = input.length();
 
-    System.out.println("Is Palindrome: " + isPalindrome);
-    System.out.println("Character Count: " + characterCount);
+    System.out.println("Apakah Palindrome: " + isPalindrome);
+    System.out.println("Jumlah Karakter: " + characterCount);
   }
 
   public static boolean checkPalindrome(String input) {
-    int length = input.length();
-    for (int i = 0; i < length / 2; i++) {
-      if (input.charAt(i) != input.charAt(length - 1 - i)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public static int countCharacters(String input) {
-    return input.length();
+    String reversedInput = new StringBuilder(input).reverse().toString();
+    return input.equalsIgnoreCase(reversedInput);
   }
 }
